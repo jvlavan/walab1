85fd70f607c3ac1eb5a83338d26640845c3580ea
Lavan JV <70334120+jvlavan@users.noreply.github.com>
2022-05-20 12:20:48 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,15 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it
+import math
+a=int(input("enter r:"))
+i=30
+x=499.0941
+z=((a**2)+(x**2))**0.5
+print("z:",z)
+c=a/z
+phi=math.acos(c)
+q=20*i*math.sin(phi)
+print("q:",q)
+print("c:",c)
+w=20*i*c
+print("w:",w)
